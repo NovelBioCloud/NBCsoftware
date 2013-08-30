a6e13d60ee5f17f08cc07b9072b4e9b0ba1eb494
zong0jie <zong0jie@gmail.com>
2013-08-30 09:37:12 +0800

    1. 上传GUI

---
+++ NBCGUI/src/main/java/com/novelbio/nbcgui/GUIinfo.java
@@ -0,0 +1,23 @@
+package com.novelbio.nbcgui;
+
+import java.util.List;
+
+import com.novelbio.base.multithread.RunProcess;
+
+public interface GUIinfo {
+	
+	void setRunningInfo(GuiAnnoInfo string);
+	
+	void done(RunProcess<GuiAnnoInfo> runProcess);
+
+	void setMessage(String string);
+
+	void setProgressBarLevelLs(List<Double> lsLevels);
+
+	void setProcessBarStartEndBarNum(String string, int level, long startBarNum, long endBarNum);
+
+	void setDetailInfo(String string);
+
+	void setInfo(String string);
+
+}
