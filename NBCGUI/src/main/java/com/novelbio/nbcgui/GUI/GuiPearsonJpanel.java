48634b40b0915b503b46dae3f965e75a93e9f196
zomg0jie <zomg0jie@gmail.com>
2013-10-21 10:02:13 +0800

    1. 修正共表达gui

--- NBCGUI/src/main/java/com/novelbio/nbcgui/GUI/GuiPearsonJpanel.java
+++ NBCGUI/src/main/java/com/novelbio/nbcgui/GUI/GuiPearsonJpanel.java
@@ -5,13 +5,9 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
-import java.util.HashMap;
 
-import javax.swing.ComboBoxModel;
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,6 +16,7 @@ import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle;
 
+import com.novelbio.analysis.coexp.simpCoExp.CoExp;
 import com.novelbio.analysis.coexp.simpCoExp.SimpCoExp;
 import com.novelbio.base.dataOperate.ExcelOperate;
 import com.novelbio.base.fileOperate.FileOperate;
@@ -52,32 +49,49 @@ public class GuiPearsonJpanel extends JPanel{
 	private JLabel jLabPathPath;
 	private JTextField jTxtFilePathPath;
 	private JButton jBtnFileOpenPath;
-	private JLabel jLabAccColPath;
-	private JTextFieldData jTxtAccColPath;
+	private JTextFieldData jTxtPvalueCufoff;
 	private JComboBoxData<Species> jCombSelSpePath;
 	private JLabel jLabPathQtaxID;
 	private JScrollPaneData jScrollPaneInputPath;
+	private JLabel lblPvaluecutoff;
 	////////////	
 	
 	public GuiPearsonJpanel() 
 	{
-		GroupLayout jPanPathLayout = new GroupLayout((JComponent)this);
 		
 
 		setPreferredSize(new java.awt.Dimension(1046, 617));
-		this.setLayout(jPanPathLayout);
 	
 		setAlignmentX(0.0f);
 		setComponent();
-		setGroup(jPanPathLayout);
+		setLayout(null);
+		add(jTxtFilePathPath);
+		add(jCombSelSpePath);
+		add(jLabPathQtaxID);
+		add(jLabPathPath);
+		add(jBtbSavePath);
+		add(jTxtPvalueCufoff);
+		add(jBtnFileOpenPath);
+		add(jLabInputReviewPath);
+		add(jScrollPaneInputPath);
+		add(jTabbedPanePathResult);
+		add(jLabResultReviewPath);
+		add(jProgressBarPath);
+		{
+			lblPvaluecutoff = new JLabel("PvalueCutOff");
+			lblPvaluecutoff.setBounds(14, 248, 129, 18);
+			add(lblPvaluecutoff);
+		}
 	}
 	private void setComponent() {
 		{
 			jLabPathQtaxID = new JLabel();
+			jLabPathQtaxID.setBounds(14, 101, 129, 18);
 			jLabPathQtaxID.setText("Query Species");
 		}
 		{
 			jTxtFilePathPath = new JTextField();
+			jTxtFilePathPath.setBounds(14, 38, 217, 24);
 			jTxtFilePathPath.addKeyListener(new KeyAdapter() {
 				public void keyTyped(KeyEvent evt) {
 					if (evt.getKeyChar() == KeyEvent.VK_ENTER) {
@@ -93,6 +107,7 @@ public class GuiPearsonJpanel extends JPanel{
 		}
 		{
 			jBtnFileOpenPath = new JButton();
+			jBtnFileOpenPath.setBounds(124, 69, 105, 25);
 			jBtnFileOpenPath.setText("LoadData");
 			jBtnFileOpenPath.setMargin(new java.awt.Insets(1, 1, 1, 1));
 			jBtnFileOpenPath.addActionListener(new ActionListener() {
@@ -111,38 +126,41 @@ public class GuiPearsonJpanel extends JPanel{
 		}
 		{
 			jLabPathPath = new JLabel();
+			jLabPathPath.setBounds(14, 13, 85, 18);
 			jLabPathPath.setText("InputData");
 		}
 		{
-			jTxtAccColPath = new JTextFieldData();
-			jTxtAccColPath.setNumOnly(5);
+			jTxtPvalueCufoff = new JTextFieldData();
+			jTxtPvalueCufoff.setBounds(14, 277, 85, 20);
+			jTxtPvalueCufoff.setNumOnly(5);
 		}
-		{
-			jLabAccColPath = new JLabel();
-			;
-			jLabAccColPath.setAlignmentY(0.0f);
-			jLabAccColPath.setName("jLabAccColPath");
-		}
+
 
 		{
 			jScrollPaneInputPath = new JScrollPaneData();
+			jScrollPaneInputPath.setBounds(245, 38, 794, 192);
 		}
 		{
 			jLabInputReviewPath = new JLabel();
+			jLabInputReviewPath.setBounds(245, 13, 97, 18);
 			jLabInputReviewPath.setText("InputReview");
 		}
 		{
 			jLabResultReviewPath = new JLabel();
+			jLabResultReviewPath.setBounds(252, 248, 127, 18);
 			jLabResultReviewPath.setText("ResultReview");
 		}
 		{
 			jProgressBarPath = new JProgressBar();
+			jProgressBarPath.setBounds(14, 617, 1025, 14);
 		}
 		{
 			jTabbedPanePathResult = new JTabbedPane();
+			jTabbedPanePathResult.setBounds(245, 273, 794, 331);
 		}
 		{
 			jBtbSavePath = new JButton();
+			jBtbSavePath.setBounds(14, 310, 92, 23);
 			jBtbSavePath.setText("Save As");
 			jBtbSavePath.setMargin(new java.awt.Insets(1, 0, 1, 0));
 			jBtbSavePath.addActionListener(new ActionListener() {
@@ -154,7 +172,8 @@ public class GuiPearsonJpanel extends JPanel{
 					}
 					try {
 						ExcelOperate excelOperate = new ExcelOperate();
-						excelOperate.openExcel(jTxtFilePathPath.getText());
+						String inputFile = jTxtFilePathPath.getText();
+						excelOperate.openExcel(inputFile);
 						int ColNum = excelOperate.getColCount(1);
 						ArrayList<String[]> aaa = excelOperate.ReadLsExcel(1, 1, 1, ColNum);
 						ColNum = 0;
@@ -167,98 +186,27 @@ public class GuiPearsonJpanel extends JPanel{
 						for (int i = 0; i < ColNum ; i++) {
 							columnID[i] = i+1;
 						}
-						SimpCoExp.getCoExpInfo(jTxtFilePathPath.getText(), columnID,9606 , Double.parseDouble(jTxtAccColPath.getText()), savefilename, false);
+						CoExp coExp = new CoExp();
+						coExp.setPvalueCutoff( Double.parseDouble(jTxtPvalueCufoff.getText()));
+						coExp.readTxtExcel(inputFile, columnID);
+						coExp.writeToExcel(savefilename);
+						SimpCoExp.getCoExpInfo(jTxtFilePathPath.getText(), columnID,9606 , Double.parseDouble(jTxtPvalueCufoff.getText()), savefilename, false);
 					} catch (Exception e) {
 						// TODO: handle exception
 					}
 				}
 			});
 		}
-		{
-			jCombSelSpePath = new JComboBoxData<Species>();
-			jCombSelSpePath.setMapItem(Species.getSpeciesName2Species(Species.KEGGNAME_SPECIES));
-		}
+		
+		jCombSelSpePath = new JComboBoxData<Species>();
+		jCombSelSpePath.setBounds(14, 126, 174, 23);
+		jCombSelSpePath.setMapItem(Species.getSpeciesName2Species(Species.KEGGNAME_SPECIES));
+		initial();
+		
 	}
 	
-	
-	private void setGroup(GroupLayout jPanPathLayout)
-	{	jPanPathLayout.setHorizontalGroup(jPanPathLayout.createSequentialGroup()
-			.addContainerGap()
-			.addGroup(jPanPathLayout.createParallelGroup()
-			    .addGroup(jPanPathLayout.createSequentialGroup()
-			        .addGroup(jPanPathLayout.createParallelGroup()
-			            .addComponent(jTxtFilePathPath, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 217, GroupLayout.PREFERRED_SIZE)
-			            .addGroup(GroupLayout.Alignment.LEADING, jPanPathLayout.createSequentialGroup()
-			                .addComponent(jCombSelSpePath, GroupLayout.PREFERRED_SIZE, 174, GroupLayout.PREFERRED_SIZE)
-			                .addGap(43))
-			            .addGroup(GroupLayout.Alignment.LEADING, jPanPathLayout.createSequentialGroup()
-			                .addComponent(jLabPathQtaxID, GroupLayout.PREFERRED_SIZE, 129, GroupLayout.PREFERRED_SIZE)
-			                .addGap(88))
-			            .addGroup(jPanPathLayout.createSequentialGroup()
-			                .addGroup(jPanPathLayout.createParallelGroup()
-			                    .addComponent(jLabAccColPath, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 104, GroupLayout.PREFERRED_SIZE)
-			                    .addGroup(GroupLayout.Alignment.LEADING, jPanPathLayout.createSequentialGroup()
-			                        .addComponent(jLabPathPath, GroupLayout.PREFERRED_SIZE, 85, GroupLayout.PREFERRED_SIZE)
-			                        .addGap(19))
-			                    .addGroup(GroupLayout.Alignment.LEADING, jPanPathLayout.createSequentialGroup()
-			                        .addComponent(jBtbSavePath, GroupLayout.PREFERRED_SIZE, 92, GroupLayout.PREFERRED_SIZE)
-			                        .addGap(12)))
-			                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-			                .addGroup(jPanPathLayout.createParallelGroup()
-			                    .addGroup(GroupLayout.Alignment.LEADING, jPanPathLayout.createSequentialGroup()
-			                        .addComponent(jTxtAccColPath, GroupLayout.PREFERRED_SIZE, 43, GroupLayout.PREFERRED_SIZE)
-			                        .addGap(62))
-			                    .addComponent(jBtnFileOpenPath, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 105, GroupLayout.PREFERRED_SIZE))))
-			        .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-			        .addGroup(jPanPathLayout.createParallelGroup()
-			            .addGroup(GroupLayout.Alignment.LEADING, jPanPathLayout.createSequentialGroup()
-			                .addComponent(jLabInputReviewPath, GroupLayout.PREFERRED_SIZE, 97, GroupLayout.PREFERRED_SIZE)
-			                .addGap(0, 697, Short.MAX_VALUE))
-			            .addGroup(jPanPathLayout.createSequentialGroup()
-			                .addComponent(jScrollPaneInputPath, GroupLayout.PREFERRED_SIZE, 794, GroupLayout.PREFERRED_SIZE)
-			                .addGap(0, 0, Short.MAX_VALUE))
-			            .addGroup(jPanPathLayout.createSequentialGroup()
-			                .addGap(0, 0, Short.MAX_VALUE)
-			                .addComponent(jTabbedPanePathResult, GroupLayout.PREFERRED_SIZE, 794, GroupLayout.PREFERRED_SIZE))
-			            .addGroup(GroupLayout.Alignment.LEADING, jPanPathLayout.createSequentialGroup()
-			                .addGap(7)
-			                .addComponent(jLabResultReviewPath, GroupLayout.PREFERRED_SIZE, 127, GroupLayout.PREFERRED_SIZE)
-			                .addGap(0, 660, Short.MAX_VALUE))))
-			    .addComponent(jProgressBarPath, GroupLayout.Alignment.LEADING, 0, 1023, Short.MAX_VALUE))
-			.addContainerGap());
-		jPanPathLayout.setVerticalGroup(jPanPathLayout.createSequentialGroup()
-				.addContainerGap()
-				.addGroup(jPanPathLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-				    .addComponent(jLabPathPath, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
-				    .addComponent(jLabInputReviewPath, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
-				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-				.addGroup(jPanPathLayout.createParallelGroup()
-				    .addComponent(jScrollPaneInputPath, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 192, GroupLayout.PREFERRED_SIZE)
-				    .addGroup(GroupLayout.Alignment.LEADING, jPanPathLayout.createSequentialGroup()
-				        .addComponent(jTxtFilePathPath, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
-				        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-				        .addComponent(jBtnFileOpenPath, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
-				        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-				        .addComponent(jLabPathQtaxID, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
-				        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-				        .addComponent(jCombSelSpePath, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
-				        .addGap(92)))
-				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-				.addComponent(jLabResultReviewPath, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
-				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-				.addGroup(jPanPathLayout.createParallelGroup()
-				    .addComponent(jTabbedPanePathResult, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 331, GroupLayout.PREFERRED_SIZE)
-				    .addGroup(GroupLayout.Alignment.LEADING, jPanPathLayout.createSequentialGroup()
-				        .addGap(10)
-				        .addGroup(jPanPathLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-				            .addComponent(jTxtAccColPath, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
-				            .addComponent(jLabAccColPath, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE))
-				        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 46, GroupLayout.PREFERRED_SIZE)
-				        .addComponent(jBtbSavePath, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
-				        .addGap(232)))
-				.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-				.addComponent(jProgressBarPath, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
-				.addGap(0, 6, Short.MAX_VALUE));
+	private void initial() {
+		jTxtPvalueCufoff.setText("0.05");
 	}
 	/**
 	 * 查看文件的鼠标或键盘事件响应时调用
@@ -269,5 +217,4 @@ public class GuiPearsonJpanel extends JPanel{
 		ArrayList<String[]> PathRawData = excelOperate.ReadLsExcel(1, 1, excelOperate.getRowCount(), excelOperate.getColCount());
 		jScrollPaneInputPath.setItemLs(PathRawData);
 	}
-	
 }
