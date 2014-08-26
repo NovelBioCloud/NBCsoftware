2e37f6dd39298ad3fec9b6f63e10355ca7fdf085
bianlianle <bianlianle@novelbio.com>
2014-08-26 18:05:10 +0800

    添加聚类其他重要参数

--- NBCGUI/src/main/java/com/novelbio/analysis/seq/rnaseq/CAP3cluster.java
+++ NBCGUI/src/main/java/com/novelbio/analysis/seq/rnaseq/CAP3cluster.java
@@ -10,7 +10,6 @@ import com.novelbio.base.fileOperate.FileOperate;
 import com.novelbio.database.domain.information.SoftWareInfo;
 import com.novelbio.database.domain.information.SoftWareInfo.SoftWare;
 
-
 /**
  * simple script : cap3 trinity.fa > trinity.cap3.result
  * @author bll
@@ -22,9 +21,16 @@ public class CAP3cluster implements IntCmdSoft {
 	
 	/** 需要聚类的序列文件，fasta格式文件 */
 	String fastaNeedCluster;
-	
 	/** 输出文件路径及名称 */
 	String outFile;
+	/** overlap区域允许的最大gap长度，默认 20 */
+	String overlapGapLen;
+	/** overlap区域长度阈值，默认 40 */
+	String overlapLenCutff;
+	/** overlap区域一致性比例，默认 90 */
+	String overlapIdePerCutff;
+	/** clip 位置reads支持数，默认 3 */
+	String readsSupportNum;
 	
 	List<String> lsCmd = new ArrayList<String>();
 	
@@ -33,30 +39,35 @@ public class CAP3cluster implements IntCmdSoft {
 		FileOperate.checkFileExistAndBigThanSize(fastaNeedCluster, 0);
 		this.fastaNeedCluster = fastaNeedCluster;
 	}
-	
-	private String[] getFastaNeedCluster(){
-		return new String[]{" ",fastaNeedCluster};
-	}
-	
 	/** 输出文件名 */
 	public void setOutFile(String outFile) {
 		this.outFile = outFile;
 	}
-	
-	private String[] getOutFile() {
-		return new String[]{">", outFile};
+	/** 设定overlap区域允许的最大gap长度  */
+	public void setOverlapGapLen(String overlapGapLen) {
+		this.overlapGapLen = overlapGapLen;
 	}
-	
+	/** 设定overlap区域长度  */
+	public void steOverlapLenCutff(String overlapLenCutff) {
+		this.overlapLenCutff = overlapLenCutff;
+	}
+	/** 设定overlap一致性比例  */
+	public void setOverlapIdePerCutff(String overlapIdePerCutff) {
+		this.overlapIdePerCutff = overlapIdePerCutff;
+	}
+	/** 设定clip 位置reads支持数  */
+	public void setReadsSupportNum(String readsSupportNum) {
+		this.readsSupportNum = readsSupportNum;
+	}
+
 	public CAP3cluster(){
-		SoftWareInfo softWareInfo = new SoftWareInfo(SoftWare.cap);
+		SoftWareInfo softWareInfo = new SoftWareInfo(SoftWare.cap3);
 		this.exePath = softWareInfo.getExePathRun();
 	}
-	
 	public void run() {
 		CmdOperate cmdOperate = new CmdOperate(getLsCmd());
 		cmdOperate.runWithExp("CAP3 error");
 	}
-	
 	private List<String> getLsCmd() {
 		List<String> lsCmd = new ArrayList<>();
 		lsCmd.add(exePath + "cap3");
@@ -64,7 +75,24 @@ public class CAP3cluster implements IntCmdSoft {
 		ArrayOperate.addArrayToList(lsCmd, getOutFile());
 		return lsCmd;
 	}
-	
+	private String[] getFastaNeedCluster(){
+		return new String[]{" ",fastaNeedCluster};
+	}
+	private String[] getOutFile() {
+		return new String[]{">", outFile};
+	}
+	private String[] getOverlapGapLen() {
+		return new String[]{"-f", overlapGapLen};
+	}
+	private String[] getOverlapLenCutff() {
+		return new String[] {"-o", overlapLenCutff};
+	}
+	private String[] getOverlapIdePerCutff() {
+		return new String[] {"-p", overlapIdePerCutff};
+	}
+	private String[] getReadsSupportNum() {
+		return new String[] {"-z", readsSupportNum};
+	}
 	public void cluster(){
 		lsCmd.clear();
 		CAP3cluster cap3cluster = new CAP3cluster();
@@ -73,7 +101,6 @@ public class CAP3cluster implements IntCmdSoft {
 		cap3cluster.run();
 		lsCmd.addAll(cap3cluster.getCmdExeStr());	
 	}
-	
 	public List<String> getCmdExeStr() {
 		List<String> lsResult = new ArrayList<String>();
 		CmdOperate cmdOperate = new CmdOperate(getLsCmd());
