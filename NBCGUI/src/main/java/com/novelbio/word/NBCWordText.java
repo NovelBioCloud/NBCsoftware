67c9d8248def891ca1e5a6727333167814b89d73
jiangpengxun <jiangpengxun@novelbio.com>
2014-12-05 20:34:43 +0800

    1. 开始重写word自动化报告模块

---
+++ NBCGUI/src/main/java/com/novelbio/word/NBCWordText.java
@@ -0,0 +1,11 @@
+package com.novelbio.word;
+
+public class NBCWordText implements NBCWordParam {
+
+	@Override
+	public void insertToDoc(Document doc, String key) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
