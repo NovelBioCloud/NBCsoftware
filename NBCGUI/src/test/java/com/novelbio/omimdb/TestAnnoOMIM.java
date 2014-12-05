cfaa862c172a3c44d8c4bae91a9fa4ee5588530e
bianlianle <bianlianle@novelbio.com>
2014-12-05 14:31:24 +0800

    1.添加omim按照基因查找基因的CtrlOMIMDisToGene的类 2.VarScan加壳，添加VarScan的CtrlVarScan.java

--- NBCGUI/src/test/java/com/novelbio/omimdb/TestAnnoOMIM.java
+++ NBCGUI/src/test/java/com/novelbio/omimdb/TestAnnoOMIM.java
@@ -2,6 +2,7 @@ package com.novelbio.omimdb;
 
 import java.util.List;
 
+import com.mongodb.util.StringParseUtil;
 import com.novelbio.analysis.annotation.genAnno.AnnoAbs;
 import com.novelbio.analysis.annotation.genAnno.AnnoOMIM;
 
@@ -10,20 +11,31 @@ import junit.framework.TestCase;
 public class TestAnnoOMIM extends TestCase {
 
 	static String genName = "ENO1";
-	static int geneId = 2023;
+	static int geneId = 3028;
 	static int taxId = 9606;
 	public static void main(String[] args) {
-		AnnoOMIM annoOMIM = new AnnoOMIM();
-		
-		List<String[]> liOmimInf = annoOMIM.getInfo(taxId, geneId+"");
-	
-		for (String[] omimInf:liOmimInf) {
-			for (int i=0; i<omimInf.length; i++) {
-				System.out.println("Test result is ===> " + omimInf[i]);
-			}
-		}
+//		AnnoOMIM annoOMIM = new AnnoOMIM();
+//		
+//		List<String[]> liOmimInf = annoOMIM.getInfo(taxId, geneId+"");
+//	
+//		for (String[] omimInf:liOmimInf) {
+//			for (int i=0; i<omimInf.length; i++) {
+//				System.out.println("Test result is ===> " + omimInf[i]);
+//			}
+//		}
+//		
+//	}
+		String testString = "111111 (2)";
+		String[] arrPhen = testString.split("\\s+");
+		int phenMimId = 0;
+		int type = 0;
+		String test1 = "";
+	if (testString.matches("^\\d{6}\\s+\\(\\d+\\)")) {
+		phenMimId = Integer.parseInt(arrPhen[0]);
 		
 	}
-
-	
+	test1 = arrPhen[arrPhen.length - 1].replaceAll("[()]", "");
+	type = Integer.parseInt(test1);
+	System.out.println(type);
+	}
 }
--- NBCGUI/src/test/java/com/novelbio/omimdb/TestGetGeneID.java
+++ NBCGUI/src/test/java/com/novelbio/omimdb/TestGetGeneID.java
@@ -5,7 +5,15 @@ import junit.framework.TestCase;
 public class TestGetGeneID  extends TestCase {
 	static String genemapFilePath = "D:\\OMIM\\genemap2_v2.txt";
 	static String inGeneIdFile = "D:\\OMIM\\omim.txt";
-	public void testGetGeneID() {
+	public static void main(String[] args) {
+//		String testString = " ";
+//		int test = Integer.parseInt(testString);
+//		System.out.println("111");
+//		System.out.println(test);
+//		System.out.println("2");
+//		String phenInfo = "Arrhythmogenic right ventricular dysplasia 3 (2)";
+//		phenInfo = phenInfo.substring(0, phenInfo.length()-3);
 		
+//		System.out.println(phenInfo);
 	}
 }
