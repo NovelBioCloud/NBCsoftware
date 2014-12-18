9e36ad75fa18a8aaf8fd178826312d43820d74b5
bianlianle <bianlianle@novelbio.com>
2014-12-18 09:13:31 +0800

    1.删除无用代码

--- NBCGUI/src/main/java/com/novelbio/nbcgui/controlseq/CtrlOMIMDisToGene.java
+++ NBCGUI/src/main/java/com/novelbio/nbcgui/controlseq/CtrlOMIMDisToGene.java
@@ -42,66 +42,66 @@ public class CtrlOMIMDisToGene {
 	}
 	
 	public void run () {
-		TxtReadandWrite txtMorbidMapRead = new TxtReadandWrite(inputFile);
-		TxtReadandWrite txtResultFileWrite = new TxtReadandWrite(outputFile, true);
-		txtResultFileWrite.writefileln(CtrlOMIMDisToGene.getTitle());
-		for (String content : txtMorbidMapRead.readlines()) {   //如何去掉第一行的Titile????
-			String[] arrLine = content.split("\t");
-			String disString = arrLine[accIDColumn - 1];
-			List<String[]> liDisToGen = CtrlOMIMDisToGene.getOMIMDisGene(disString);
-			for (String[] contentString : liDisToGen) {
-				txtResultFileWrite.writefileln(contentString);
-			}
-		}
-		txtMorbidMapRead.close();
-		txtResultFileWrite.close();
+//		TxtReadandWrite txtMorbidMapRead = new TxtReadandWrite(inputFile);
+//		TxtReadandWrite txtResultFileWrite = new TxtReadandWrite(outputFile, true);
+//		txtResultFileWrite.writefileln(CtrlOMIMDisToGene.getTitle());
+//		for (String content : txtMorbidMapRead.readlines()) {   //如何去掉第一行的Titile????
+//			String[] arrLine = content.split("\t");
+//			String disString = arrLine[accIDColumn - 1];
+//			List<String[]> liDisToGen = CtrlOMIMDisToGene.getOMIMDisGene(disString);
+//			for (String[] contentString : liDisToGen) {
+//				txtResultFileWrite.writefileln(contentString);
+//			}
+//		}
+//		txtMorbidMapRead.close();
+//		txtResultFileWrite.close();
 	}
 	//此处这个方法可以与AnnoOMIM.java类型的方法，进行合并，另外写一个类，运用多态的性能即可！
-	public static List<String[]> getOMIMDisGene(String dis) {
-		MgmtMorbidMap mgmtMorbidMap = MgmtMorbidMap.getDispatch();
-		MgmtOMIMUnit mgmtOMIMUnit = MgmtOMIMUnit.getDispatch();
-		List<String[]> lsResult = new ArrayList<String[]>();
-		ArrayList<String> lsResultTmp = new ArrayList<String>();
-		List<MorbidMap> liMorbidMap = mgmtMorbidMap.findInfByDisease(dis);
-		lsResultTmp.add(dis);
-		if (liMorbidMap.size() == 0) {
-			fillLsResult(dis + "", lsResult, 9);	
-			return lsResult;
-		}
-		for (MorbidMap morbidMap : liMorbidMap) {
-			ArrayList<String> lsTmp = (ArrayList<String>) lsResultTmp.clone();
-			//TODO此处换成Gene Name
-			lsTmp.add(morbidMap.getGeneId() + "");  
-			lsTmp.add(morbidMap.getGeneMimId() + "");
-			if (morbidMap.getGeneMimId() != 0) {
-				MIMInfo mimInfo = mgmtOMIMUnit.findByMimId(morbidMap.getGeneMimId());
-				lsTmp.add(mimInfo.getDesc());
-				List<String> listRef = mimInfo.getListRef();
-				lsTmp.add(listRef.get(0));
-			}
-			if (morbidMap.getPheneMimId() == 0) {
-				lsTmp.add("");
-				lsTmp.add("");
-				lsTmp.add("");
-			} else {
-				lsTmp.add(morbidMap.getPheneMimId() + "");
-			}
-			if (morbidMap.getPheneMimId() != 0) {
-				MIMInfo mimInfo = mgmtOMIMUnit.findByMimId(morbidMap.getPheneMimId());
-				lsTmp.add(mimInfo.getDesc());
-				List<String> listRef = mimInfo.getListRef();
-				lsTmp.add(listRef.get(0));
-			}
-			List<String> listDis = morbidMap.getListDis();
-			String disease = "";
-			for (String disContent : listDis) {
-				disease = disease.concat(disContent + "");
-			}
-			lsTmp.add(morbidMap.getDisType() + "");
-			lsResult.add(lsTmp.toArray(new String[0]));
-		}
-		return lsResult;
-	}
+//	public static List<String[]> getOMIMDisGene(String dis) {
+//		MgmtMorbidMap mgmtMorbidMap = MgmtMorbidMap.getDispatch();
+//		MgmtOMIMUnit mgmtOMIMUnit = MgmtOMIMUnit.getDispatch();
+//		List<String[]> lsResult = new ArrayList<String[]>();
+//		ArrayList<String> lsResultTmp = new ArrayList<String>();
+//		List<MorbidMap> liMorbidMap = mgmtMorbidMap.findInfByDisease(dis);
+//		lsResultTmp.add(dis);
+//		if (liMorbidMap.size() == 0) {
+//			fillLsResult(dis + "", lsResult, 9);	
+//			return lsResult;
+//		}
+//		for (MorbidMap morbidMap : liMorbidMap) {
+//			ArrayList<String> lsTmp = (ArrayList<String>) lsResultTmp.clone();
+//			//TODO此处换成Gene Name
+//			lsTmp.add(morbidMap.getGeneId() + "");  
+//			lsTmp.add(morbidMap.getGeneMimId() + "");
+//			if (morbidMap.getGeneMimId() != 0) {
+//				MIMInfo mimInfo = mgmtOMIMUnit.findByMimId(morbidMap.getGeneMimId());
+//				lsTmp.add(mimInfo.getDesc());
+//				List<String> listRef = mimInfo.getListRef();
+//				lsTmp.add(listRef.get(0));
+//			}
+//			if (morbidMap.getPheneMimId() == 0) {
+//				lsTmp.add("");
+//				lsTmp.add("");
+//				lsTmp.add("");
+//			} else {
+//				lsTmp.add(morbidMap.getPheneMimId() + "");
+//			}
+//			if (morbidMap.getPheneMimId() != 0) {
+//				MIMInfo mimInfo = mgmtOMIMUnit.findByMimId(morbidMap.getPheneMimId());
+//				lsTmp.add(mimInfo.getDesc());
+//				List<String> listRef = mimInfo.getListRef();
+//				lsTmp.add(listRef.get(0));
+//			}
+//			List<String> listDis = morbidMap.getListDis();
+//			String disease = "";
+//			for (String disContent : listDis) {
+//				disease = disease.concat(disContent + "");
+//			}
+//			lsTmp.add(morbidMap.getDisType() + "");
+//			lsResult.add(lsTmp.toArray(new String[0]));
+//		}
+//		return lsResult;
+//	}
 	private static void fillLsResult(String accID, List<String[]> lsResult, int arrayLength) {
 		String[] tmpResult = new String[arrayLength];
 		tmpResult[0] = accID;
